163801927955b377ec7eb85aabbd4a78689dc2ea
Yash Garg <thisisyashgarg@gmail.com>
2024-08-22 23:56:51 +0530

    docs: fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,50 +49,53 @@ const Home = () => {
   return (
     <div className="min-h-screen bg-[#212529] flex flex-col items-center justify-center p-4 font-roboto-mono text-[#E9ECEF]">
       <div className="bg-[#343A40] shadow-md rounded-lg p-6 max-w-4xl w-full md:w-11/12 sm:w-full">
-        <h1 className="text-2xl font-semibold text-[#F8F9FA] mb-4 text-center">
-          cURL to Request/Response Types
+        <h1 className="text-3xl font-semibold text-[#F8F9FA] mb-4 text-center">
+          Typu
         </h1>
-        <form>
-          <textarea
-            value={curlCommand}
-            onChange={(e) => setCurlCommand(e.target.value)}
-            rows={5}
-            className="w-full p-3 border border-[#495057] bg-[#212529] rounded-md focus:outline-none focus:ring-2 focus:ring-[#495057] text-[#F8F9FA]"
-            placeholder="Paste your cURL command here"
-          />
-          <div className="flex justify-center mt-4">
-            <button
-              onClick={handleCurlSubmit}
-              type="button"
-              className="bg-[#495057] hover:bg-[#6C757D] text-[#F8F9FA] font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-[#ADB5BD]"
-            >
-              {loading ? (
-                <svg
-                  className="animate-spin h-5 w-5 text-[#F8F9FA]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v8H4z"
-                  ></path>
-                </svg>
-              ) : (
-                "Submit"
-              )}
-            </button>
-          </div>
-        </form>
+        <p className="text-lg text-[#F8F9FA] mb-6 text-center">
+          A simple tool to generate TypeScript types from cURL commands.
+        </p>
+
+        <textarea
+          value={curlCommand}
+          onChange={(e) => setCurlCommand(e.target.value)}
+          rows={5}
+          className="w-full p-3 border border-[#495057] bg-[#212529] rounded-md focus:outline-none focus:ring-2 focus:ring-[#495057] text-[#F8F9FA]"
+          placeholder="Paste your cURL command here"
+        />
+        <div className="flex justify-center mt-4">
+          <button
+            onClick={handleCurlSubmit}
+            type="button"
+            className="bg-[#495057] hover:bg-[#6C757D] text-[#F8F9FA] font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-[#ADB5BD]"
+          >
+            {loading ? (
+              <svg
+                className="animate-spin h-5 w-5 text-[#F8F9FA]"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v8H4z"
+                ></path>
+              </svg>
+            ) : (
+              "Generate Types"
+            )}
+          </button>
+        </div>
+
         {errorMessage && (
           <div className="mt-4 text-[#DE3545] text-center">{errorMessage}</div>
         )}
